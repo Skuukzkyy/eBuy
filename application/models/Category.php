2cb0e0733ff1115d373956177b479752948bfd0c
Skuukzkyy <109440860+Skuukzkyy@users.noreply.github.com>
2023-02-20 10:11:46 +0800

    changed table name from categories to product_categories

--- application/models/Category.php
+++ application/models/Category.php
@@ -4,10 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Category extends CI_Model {
 
     function get_all(){
-        $query = "SELECT categories.id, categories.name, COUNT(*) as product_count 
-                    FROM v88_ebuy.categories
-                    INNER JOIN products ON products.category_id = categories.id
-                    GROUP BY categories.id
+        $query = "SELECT product_categories.id, product_categories.name, COUNT(*) as product_count 
+                    FROM product_categories
+                    INNER JOIN products ON products.category_id = product_categories.id
+                    GROUP BY product_categories.id
                 ";
         return $this->db->query($query)->result_array();
     }
